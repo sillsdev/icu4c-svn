a8fb9304315d06ea56e48dc1e8547cf0006475eb
DALLAS\Thomson <john_thomson@sil.org>
2017-05-04 10:22:07 -0500

    Set up the minimal build with smaller icudt58.dll
    Also: add gitignore
    Fix bugs in makedata.mak when removing resources and not adding any

---
+++ source/data/translit/trnslocal.mk
@@ -0,0 +1,11 @@
+# Copyright (C) 2016 and later: Unicode, Inc., SIL, and others.
+# License & terms of use: http://www.unicode.org/copyright.html
+# A list of txt's to build
+#
+#  * To REPLACE the default list and only build with a few
+#     transliterators did something like this:
+#    _____________________________________________________
+#    |  TRANLIST_SOURCE = el.txt th.txt
+
+TRANSLIT_SOURCE= el.txt
+TRANSLIT_SOURCE_LOCAL = en.txt
